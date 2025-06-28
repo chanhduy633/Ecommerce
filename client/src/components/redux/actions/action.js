4f5d720f3f1493bbe6fd4ae0cd0ec8eeeec49c36
DUY LC <118537829+chanhduy633@users.noreply.github.com>
2025-06-28 18:16:56 +0700

    Update action.js

--- client/src/components/redux/actions/action.js
+++ client/src/components/redux/actions/action.js
@@ -1,6 +1,8 @@
+const API_URL = "https://ecommerce-backend-sm3l.onrender.com";
+
 export const getProducts = ()=> async(dispatch)=>{
     try {
-        const data = await fetch("/getproducts",{
+        const data = await fetch(`${API_URL}/getproducts`,{
                 method:"GET",
                 headers:{
                     "Content-Type":"application/json"
